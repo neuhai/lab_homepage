767964cc6506f2c286058e77038be6433d30129f
Leo Lu <luyuxuanleo@gmail.com>
2024-02-03 19:32:20 -0500

    fix table flashing white

--- assets/js/no_defer.js
+++ assets/js/no_defer.js
@@ -1,21 +1,14 @@
 // add bootstrap classes to tables
-$(document).ready(function() {
-  $('table').each(function() {
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("table").forEach(function (table) {
     if (document.documentElement.getAttribute("data-theme") == "dark") {
-      $(this).addClass('table-dark');
+      table.classList.add("table-dark");
     } else {
-      $(this).removeClass('table-dark');
+      table.classList.remove("table-dark");
     }
-
-    // only select tables that are not inside an element with "news" (about page) or "card" (cv page) class
-    if($(this).parents('[class*="news"]').length==0 &&
-        $(this).parents('[class*="card"]').length==0 &&
-        $(this).parents('code').length == 0) {
-      // make table use bootstrap-table
-      $(this).attr('data-toggle','table');
-      // add some classes to make the table look better
-      // $(this).addClass('table-sm');
-      $(this).addClass('table-hover');
+    if (table.parentElement.classList.contains("news") || table.parentElement.classList.contains("card") || table.parentElement.tagName == "CODE") {
+      this.setAttribute("data-toggle", "table");
+      this.classList.add("table-hover");
     }
   })
 });
